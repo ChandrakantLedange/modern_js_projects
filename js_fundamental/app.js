6f490c2d28797cfad9f448f9a68921d733ec5713
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2022-05-28 09:02:08 +0530

    js fundamental

---
+++ js_fundamental/app.js
@@ -0,0 +1,59 @@
+//log to console
+console.log("Hello from app file");
+console.log(123);
+console.log([10,20,30]);
+console.log({a:11,b:22});
+console.table({a:111,b:222});
+console.error('this is some error');
+console.warn("this is warning");
+console.time("time start");
+console.log("count the time");
+console.log("count the time");
+console.log("count the time");
+console.log("count the time");
+console.log("count the time");
+console.log("count the time");
+console.log("count the time");
+console.timeEnd("time start");
+// console.clear();
+
+//comment 
+// single line comment
+/*
+multi
+line comment
+*/
+
+
+//Variables
+//var, let and const
+
+// var name = "Ganesh";
+// console.log(name);
+// name = "Mangesh";
+// console.log(name);
+
+//Initialization var //this is mostly used when we are working with switch statement
+// var greeting;
+// console.log(greeting); //undefined
+// greeting= "Good morning";
+// console.log(greeting);
+
+
+//rules for variable creation
+//letters , numbers , _  and $
+//can not starts with number
+
+
+//LET
+let name = "Ganesh";
+console.log(name);
+name = "Mangesh";
+console.log(name);
+
+
+//const 
+const age = 26;
+console.log(age);
+// age=28;   //can't reassign
+// const lang; // have to aasign a value
