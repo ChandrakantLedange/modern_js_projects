0c91cffa222cbce0fae9a6f65a09b2f7965fdfaa
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2022-05-31 08:06:29 +0530

    Data types

--- js_fundamental/app.js
+++ js_fundamental/app.js
@@ -74,3 +74,49 @@ console.log(age);
 //I)Array
 //II)Object
 //III)FUnctions ... etc
+
+
+// ** Type conversion ** 
+let val ;
+
+//Number to string
+val = String(555);
+val = String(4+4);
+
+//Boolean to string
+val = String(true);
+
+//date to string
+val = String(new Date());
+
+//Array to string
+val = String([1,2,3,4]);// lenth will count with comma(,)//7
+
+//toString()
+val = (5).toString();
+val = (true).toString();
+
+//string to number 
+val = Number('5'); 
+// console.log(val.toFixed(2));//5.00
+val = Number(true); //1
+val = Number(false);//0 
+val = Number(null);//0 
+val = Number('hello');//NaN
+val = Number([1,2,3,4]);//NaN
+
+val = parseInt('100.30');//100
+val = parseFloat('100.30');//100.3
+
+//output
+// console.log(val); 
+// console.log(typeof val); 
+// console.log(val.length); 
+
+
+// const val1 =5;
+const val1 =String(5);
+const val2 = 6;
+const sum = val1 + val2;
+console.log(sum);
+console.log(typeof sum);
